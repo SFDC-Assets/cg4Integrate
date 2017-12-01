ca4026f9be20a125a7f2f7fad8f837c09b03722f
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-01 08:08:03 -0600

    initial commit

---
+++ orgInit.sh
@@ -0,0 +1,7 @@
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1
+sfdx force:source:push
+sfdx force:user:permset:assign -n Ducati_Dealer_Manager
+sfdx force:data:tree:import -f data/Account.json
+
+sfdx force:user:password:generate
+sfdx force:org:open
