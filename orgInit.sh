02334b2799b40fa6e7770056d2ed5dc043c18ee4
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-06 09:28:02 -0600

    description in def, longer lives

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 1
+sfdx force:org:create -f config/project-scratch-def.json -s -d 4
 sfdx force:source:push
 sfdx force:user:permset:assign -n Ducati_Dealer_Manager
 sfdx force:data:tree:import -f data/Account.json
