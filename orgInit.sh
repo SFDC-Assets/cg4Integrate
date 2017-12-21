c888d94384ebc0f39d0b4a630058416669402ef5
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-21 16:54:42 -0600

    testing the whitelist

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,10 @@
+echo "this is a non-sfdx command"
 sfdx force:org:create -f config/project-scratch-def.json -s -d 4
 sfdx force:source:push
 sfdx force:user:permset:assign -n Ducati_Dealer_Manager
 sfdx force:data:tree:import -f data/Account.json
+
+ls
 
 sfdx force:user:password:generate
 sfdx force:org:open -p /one/one.app#/setup/ExternalDataSource/home
