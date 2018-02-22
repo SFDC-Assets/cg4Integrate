45eb22a50a18986ba0746200bfe0592d18b31ab7
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-02-22 16:55:09 -0600

    create an easy password

--- orgInit.sh
+++ orgInit.sh
@@ -1,10 +1,6 @@
-echo "this is a non-sfdx command"
 sfdx force:org:create -f config/project-scratch-def.json -s -d 4
 sfdx force:source:push
 sfdx force:user:permset:assign -n Ducati_Dealer_Manager
-sfdx force:data:tree:import -f data/Account.json
-
-ls
-
-sfdx force:user:password:generate
+sfdx msm:user:password:set -g User -l User -p sfdx1234
 sfdx force:org:open -p /one/one.app#/setup/ExternalDataSource/home
+sfdx force:data:tree:import -f data/Account.json
