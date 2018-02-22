fe56f9be26acefb93b1b23083745c4db56078b19
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-02-22 16:59:37 -0600

    friendly usernames FTW

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 4
+sfdx msm:org:create -f config/project-scratch-def.json -d 4 -s --json -u int -o camp.booth
 sfdx force:source:push
 sfdx force:user:permset:assign -n Ducati_Dealer_Manager
 sfdx msm:user:password:set -g User -l User -p sfdx1234
